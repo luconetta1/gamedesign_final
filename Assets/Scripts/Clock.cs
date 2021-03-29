9544c3b02e4208c22a84338c398a6d9e3781c3c5
luconetta1 <luconetta1@gmail.com>
2021-03-29 16:51:15 -0400

    Scripts

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using System;
+
+public class Clock: MonoBehaviour {
+  
+    private Text timeClock;
+    void Start (){
+        timeClock = GetComponent<Text>();
+    }
+    void Update (){
+        DateTime time = DateTime.Now;
+        int hour =time.Hour;
+        if( hour > 12){
+            hour = hour - 12;
+        }
+        string hour_fin =hour.ToString() ;
+        string minute = LeadingZero( time.Minute );
+        //Console.Error.WriteLine(Clock.text);
+        timeClock.text = hour_fin + ":" + minute;
+    }
+    string LeadingZero (int n){
+        return n.ToString().PadLeft(2, '0');
+    }
+}
---
+++ Assets/Scripts/Time.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Time: MonoBehaviour {
+  
+    private Text Clock;
+    void Awake (){
+        Clock = GetComponent<Text>();
+    }
+    void Update (){
+        DateTime time = DateTime.Now;
+        string hour = LeadingZero( time.Hour );
+        string minute = LeadingZero( time.Minute );
+        //Clock.text = hour + ":" + minute;
+    }
+    string LeadingZero (int n){
+        return n.ToString().PadLeft(2, '0');
+    }
+}
