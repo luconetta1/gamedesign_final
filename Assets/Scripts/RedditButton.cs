f3902138cbdff817c2af4ff879d32268a4a1ae5b
luconetta1 <luconetta1@gmail.com>
2021-04-04 14:50:01 -0400

    reddit-part1

--- Assets/Scripts/RedditButton.cs
+++ Assets/Scripts/RedditButton.cs
@@ -3,8 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
 public class RedditButton : MonoBehaviour
 {
+
+  public int b1 = 0;
+  public int b2 = 0;
+  public int b3 = 0;
+  public int b4 = 0;
+  
+
+  void Update(){
+    if(b1 == 1 && b2 == 1 && b3 == 1 && b4 == 1){
+      NextScene();
+    }
+  }
+
   public void NextScene()
   {
       int y = SceneManager.GetActiveScene().buildIndex;
@@ -18,4 +33,38 @@ public class RedditButton : MonoBehaviour
         SceneManager.LoadScene(3);
       }
   }
+
+  public void setb1(){
+    b1 = 1;
+  }
+
+  public void resetb1(){
+    b1 = 0;
+  }
+
+   public void setb2(){
+    b2 = 1;
+  }
+
+  public void resetb2(){
+    b2 = 0;
+  }
+
+   public void setb3(){
+    b3 = 1;
+  }
+
+  public void resetb3(){
+    b3 = 0;
+  }
+
+   public void setb4(){
+    b4 = 1;
+  }
+
+  public void resetb4(){
+    b4 = 0;
+  }
 }
+
+
