2c90d6ad970e8a61c95160a59fad748e45647f9d
luconetta1 <luconetta1@gmail.com>
2021-04-19 18:53:58 -0400

    message

---
+++ Assets/Scripts/message.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class message : MonoBehaviour
+{
+
+    public static GameObject message1;
+    public static GameObject message2;
+    public static GameObject message3;
+    //can use more/less messages depending on scene
+
+   // public AudioSource ding;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        message1 = GameObject.Find("message1");
+        message2 = GameObject.Find("message2");
+        message3 = GameObject.Find("message3");
+        
+        message1.SetActive(false); 
+        message2.SetActive(false); 
+        message3.SetActive(false); 
+
+
+        Invoke("msg1", 1.0f);
+    }
+    
+
+    public void msg1()
+    {
+        message1.SetActive(true);
+       //  ding.Play();
+    }
+}
