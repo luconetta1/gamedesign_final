900adeb7730186aa5030ab9b7d22ee06456ff739
jlnordlund <60247191+jlnordlund@users.noreply.github.com>
2021-04-29 16:07:03 -0400

    random snap name order

--- Assets/Scripts/snapchat1.cs
+++ Assets/Scripts/snapchat1.cs
@@ -7,9 +7,16 @@ public class snapchat1 : MonoBehaviour
 {
     public GameObject popup;
     //public AudioSource ding;
+    public GameObject ans1;
+    public GameObject ans2;
+    public GameObject correctAns;
+    public GameObject ans4;
+    public GameObject ans5;
+    public GameObject ans6;
     
     void Start()
     {
+        randomOrder();
         popup = GameObject.Find("popup");
         popup.SetActive(false); 
 
@@ -43,6 +50,29 @@ public class snapchat1 : MonoBehaviour
       }
       else {
         SceneManager.LoadScene("Message5"); //fifth message
+      }
+    }
+    
+    private void randomOrder() {
+      Vector3 pos1 = ans1.transform.position;
+      Vector3 pos2 = ans2.transform.position;
+      Vector3 pos3 = correctAns.transform.position;
+      Vector3 pos4 = ans4.transform.position;
+      Vector3 pos5 = ans5.transform.position;
+      Vector3 pos6 = ans6.transform.position;
+      
+      Vector3[] positions = new Vector3[]{pos1,pos2,pos3,pos4,pos5,pos6};
+      bool[]    beenUsed  = new bool[]{false,false,false,false,false,false,true};
+      GameObject[] buttons = new GameObject[]{ans1,ans2,correctAns,ans4,ans5,ans6};
+      
+      for (int i = 0; i < buttons.Length; i++) {
+        int j = 6;
+        while (beenUsed[j]) {
+         j = UnityEngine.Random.Range(0,positions.Length);
+        }
+        buttons[i].transform.position = positions[j];
+        beenUsed[j] = true;
+        Debug.Log(i);
       }
     }
 }
