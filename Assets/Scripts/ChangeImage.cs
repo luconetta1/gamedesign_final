f421390ff3eefb13029bc93b5e37365314cbc11a
jlnordlund <60247191+jlnordlund@users.noreply.github.com>
2021-05-09 23:35:02 -0400

    random insta levels

--- Assets/Scripts/ChangeImage.cs
+++ Assets/Scripts/ChangeImage.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class ChangeImage : MonoBehaviour
 {
@@ -9,50 +11,89 @@ public class ChangeImage : MonoBehaviour
     public Sprite image2;
     public Sprite image3;
     public Sprite image4;
+    
+    // alternatives for images with hidden button
+    public Sprite search;
+    public Sprite shop;
+    public Sprite home;
+    public Sprite like;
+    
     public Image post;
     
+    private static int i;
     private static int postNum;
+    private Sprite[] posts   = new Sprite[4];
+    private string[] options = new string[]{"Search","Shop","Home","Like"};
     
-    void Start() {
-      post.sprite = image1;
-      postNum = 1;
+    void Start() {  
+      string currScene = SceneManager.GetActiveScene().name;
+      if (currScene == "Instagram2") {
+        Sprite[] pics1 = new Sprite[]{search,shop,home,like};
+        Sprite[] pics2 = new Sprite[]{image1,image2,image3,image4};
+      
+        for (int j = 0; j < 4; j++) {
+          if (i == j) {
+            posts[j] = pics1[j];
+          }
+          else {
+            posts[j] = pics2[j];
+          }
+        }
+        
+        post.sprite = posts[0];
+        postNum = 1;
+      }
+    }
+    
+    public void setI() {
+      i  = UnityEngine.Random.Range(0,options.Length);
     }
     
     public void nextImage() {
       if (postNum == 1) {
-        post.sprite = image2;
+        post.sprite = posts[1];
         postNum = 2;
       }
       else if (postNum == 2) {
-        post.sprite = image3;
+        post.sprite = posts[2];
         postNum = 3;
       }
       else if (postNum == 3) {
-        post.sprite = image4;
+        post.sprite = posts[3];
         postNum = 4;
       }
       else if (postNum == 4) {
-        post.sprite = image1;
+        post.sprite = posts[0];
         postNum = 1;
       }
     }
     
     public void lastImage() {
       if (postNum == 1) {
-        post.sprite = image4;
+        post.sprite = posts[3];
         postNum = 4;
       }
       else if (postNum == 2) {
-        post.sprite = image1;
+        post.sprite = posts[0];
         postNum = 1;
       }
       else if (postNum == 3) {
-        post.sprite = image2;
+        post.sprite = posts[1];
         postNum = 2;
       }
       else if (postNum == 4) {
-        post.sprite = image3;
+        post.sprite = posts[2];
         postNum = 3;
       }
+    }
+    
+    public void nextScene() {
+      if (EventSystem.current.currentSelectedGameObject.name == options[i]) {
+          SceneManager.LoadScene("Message6");  //6th message
+      }
+      else {
+        SceneManager.LoadScene("Lose");  // lose scene
+      }
+    
     }
 }
--- Assets/Scripts/InstaButtons.cs
+++ Assets/Scripts/InstaButtons.cs
@@ -3,24 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class InstaButtons : MonoBehaviour
 {
   
     public AudioSource click;
+    
+    public Sprite search;
+    public Sprite shop;
+    public Sprite home;
+    public Sprite comment;
+    public Sprite like;
+    public Image post;
+    
+    private string hidden;
+    private static int i;
+    private string[] options = new string[]{"Search","Shop","Home","Comment","Like"};
+    
+    void Start() {
+      string currScene = SceneManager.GetActiveScene().name;
+      if (currScene == "Instagram") {
+        hidden = options[i];
+        
+        Sprite[] pics = new Sprite[]{search,shop,home,comment,like};
+        post.sprite = pics[i];
+        
+      }
+    }
+    
+    public void setI() {
+      i  = UnityEngine.Random.Range(0,options.Length);
+      Debug.Log(i);
+    }
   
     public void findNextScene() {
       if (SceneManager.GetActiveScene().name == "Instagram") {
-        if (EventSystem.current.currentSelectedGameObject.name == "Comment") {
+        if (EventSystem.current.currentSelectedGameObject.name == hidden) {
             SceneManager.LoadScene("Message2");  //second message scene
-        }
-        else {
-          SceneManager.LoadScene("Lose");  // lose scene
-        }
-      }
-      else {
-        if (EventSystem.current.currentSelectedGameObject.name == "Like") {
-            SceneManager.LoadScene("Message6");  //6th message
         }
         else {
           SceneManager.LoadScene("Lose");  // lose scene
